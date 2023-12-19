784bf2ef18983234251f709e927bc7a8a8e5c576
Jasonchrism <jasoncm712@gmail.com>
2023-12-20 06:33:49 +0700

    update report blade

--- resources/views/penghuni/report.blade.php
+++ resources/views/penghuni/report.blade.php
@@ -20,31 +20,33 @@
     <div class="container-content">
         <form action="/penghuni/report" method="post" enctype="multipart/form-data">
             @csrf
-            <div class="wrap-type">
-                <h3>Type</h3>
-                <div class="wrap-allfasum">
-                    <div class="wrap-fasum">
-                        <input type="radio" name="report_type" value="Public Facility" required>
-                        <p>Fasilitas Umum</p>
-                    </div>
-                    <div class="wrap-fasum">
-                        <input type="radio" name="report_type" value="Room" required>
-                        <p>Cluster/Kamar</p>
+            <div class="wrap-all">
+                <div class="wrap-type">
+                    <h3>Type</h3>
+                    <div class="wrap-allfasum">
+                        <div class="wrap-fasum">
+                            <input type="radio" name="report_type" value="Public Facility" required>
+                            <p>Fasilitas Umum</p>
+                        </div>
+                        <div class="wrap-fasum">
+                            <input type="radio" name="report_type" value="Room" required>
+                            <p>Cluster/Kamar</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="wrap-report">
-                <h3>Laporan / Keluhan</h3>
-                <textarea name="report" id="" cols="100" rows="15" placeholder="Jelaskan kerusakan yang dialami..." class="report-textarea" required></textarea>
-            </div>
-            <div class="wrap-photo-upload">
-                <h3>Unggah Foto</h3>
-                <div class="wrap-photo">
-                    <input name="photo" type="file" class="input-photo" required>
+                <div class="wrap-report">
+                    <h3>Laporan / Keluhan</h3>
+                    <textarea name="report" id="" cols="100" rows="15" placeholder="Jelaskan kerusakan yang dialami..." class="report-textarea" required></textarea>
                 </div>
-            </div>
-            <div class="wrap-submit">
-                <button type="submit">Submit</button>
+                <div class="wrap-photo-upload">
+                    <h3>Unggah Foto</h3>
+                    <div class="wrap-photo">
+                        <input name="photo" type="file" class="input-photo" required>
+                    </div>
+                </div>
+                <div class="wrap-submit">
+                    <button type="submit">Submit</button>
+                </div>
             </div>
         </form>
     </div>
