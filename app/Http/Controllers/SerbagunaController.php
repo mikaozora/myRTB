81273a96b63dbe896115b3964a5c42f052b02262
BintangAryo26 <bintangaryo08@gmail.com>
2024-01-04 08:40:26 +0700

    update responsive theatre

--- app/Http/Controllers/SerbagunaController.php
+++ app/Http/Controllers/SerbagunaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class SerbagunaController extends Controller
@@ -15,12 +16,62 @@ class SerbagunaController extends Controller
 
         if($request->is('dashboard/*')){
             return response()->view('dashboard.serbaguna', [
-                "title" => "Booking Serbaguna",
+                "title" => "Booking Ruang Serbaguna",
                 "photoProfile" => $photoProfile
             ]);
         }
+
+        $date = [];
+        for ($i = 0 ; $i <=6 ; $i++){
+            $carbonInstance = Carbon::createFromFormat('Y-m-d H:i:s' , Carbon::now()->addDay($i));
+            $res = $carbonInstance->format('Y-m-d');
+            $date[] = [
+                "label" => Carbon::now()->addDay($i)->locale('id_ID')->format('D,d'),
+                "value" => $res
+            ];
+        }
+
+        
+        $timeParam = $request->get('time');
+        $dateParam = $request->get('date');
+        $datetime = $dateParam . ' ' . $timeParam;
+
+        $timeAvail = [];
+        $timeNow = Carbon::now()->timezone('Asia/Jakarta')->format('H');
+        $dateNow = Carbon::now()->timezone('Asia/Jakarta')->format('Y-m-d');
+
+        for ($i = 0; $i <= 22; $i += 2) {
+            $isNow = false;
+            if ($dateParam == $dateNow) {
+                $isNow = true;
+            }
+            if ($isNow) {
+                if ($i > $timeNow) {
+                    $timeAvail[] = [
+                        "label" => str_pad($i, 2, '0', STR_PAD_LEFT) . '.00 - ' . ($i == 22 ? str_pad(0, 2, '0', STR_PAD_LEFT) . '.00' : str_pad($i + 2, 2, '0', STR_PAD_LEFT) . '.00'),
+                        "value" => str_pad($i, 2, '0', STR_PAD_LEFT) . ':00:00',
+                        "isAvailable" => true
+                    ];
+                } else {
+                    $timeAvail[] = [
+                        "label" => str_pad($i, 2, '0', STR_PAD_LEFT) . '.00 - ' . ($i == 22 ? str_pad(0, 2, '0', STR_PAD_LEFT) . '.00' : str_pad($i + 2, 2, '0', STR_PAD_LEFT) . '.00'),
+                        "value" => str_pad($i, 2, '0', STR_PAD_LEFT) . ':00:00',
+                        "isAvailable" => false
+                    ];
+                }
+            } else {
+                $timeAvail[] = [
+                    "label" => str_pad($i, 2, '0', STR_PAD_LEFT) . '.00 - ' . ($i == 22 ? str_pad(0, 2, '0', STR_PAD_LEFT) . '.00' : str_pad($i + 2, 2, '0', STR_PAD_LEFT) . '.00'),
+                    "value" => str_pad($i, 2, '0', STR_PAD_LEFT) . ':00:00',
+                    "isAvailable" => true
+                ];
+            }
+        }
+
         return response()->view('penghuni.serbaguna', [
-            "title" => "Booking Serbaguna",
+            "title" => "Booking Ruang Serbaguna",
+            "datenow" => $date,
+            "timeAvail" => $timeAvail,
             "photoProfile" => $photoProfile
         ]);
     }
--- resources/views/penghuni/serbaguna.blade.php
+++ resources/views/penghuni/serbaguna.blade.php
@@ -1,17 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{$title}}</title>
+    @vite('resources/css/app.css')
+    @vite('resources/css/sergun.css')
     @vite('resources/css/content.css')
+    <title>{{$title}}</title>
 </head>
+
 <body>
+    @if(session('message'))
+    @include('components.notification')
+    @endif
     @include('components.sidebaruser')
-    <div class="container-content">
+    <div class="kontainer-header">
         @include('components.headercontent')
-        
     </div>
+
+    <div class="container-content">
+        <div class="wrap-content">
+            <div class="wrap-left">
+                @include('components.date')
+                <hr>
+                <form action="/penghuni/serbaguna?date={{ Request::get('date')}}" method="post">
+                    @csrf
+                    <div class="wrap-validate-choose-date">
+                        @if(!Request::get('date'))
+                        <div class="wrap-span">
+                            <img src="{{ asset('/assets/ill-calendar.svg') }}" alt="">
+                            <h6>Pilih tanggal dulu yuk</h6>
+                        </div>
+                        @else
+                        <h4>Pilih Jam Booking</h4>
+
+                        @endif
+                    </div>
+</div>
+
 </body>
 </html>
